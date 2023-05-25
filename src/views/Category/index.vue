7ab4377d1c37ad4a8d2fdc48b5b5d9ff77713e5a
zhangqi <1619963687@qq.com>
2023-05-25 16:27:17 +0800

    router基本设置

---
+++ src/views/Category/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        我是category
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        我是首页
+        <!-- 二级路由出口 -->
+        <RouterView />
+        <!-- <RouterLink to="" /> -->
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
