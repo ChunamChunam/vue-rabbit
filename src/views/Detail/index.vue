1adf265ddd61655a61c34f4ffcff39dfdf1fb0f9
zhangqi <1619963687@qq.com>
2023-06-03 12:21:46 +0800

    详情热榜-基础组件封装和基础数据获取渲染

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -2,7 +2,7 @@
 import { getDetail } from '@/apis/detail';
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
-
+import DetailHot from './components/detailHot.vue'
 
 const goods = ref({})
 const route = useRoute()
@@ -21,11 +21,11 @@ onMounted(() => getGoods())
                 <el-breadcrumb separator=">">
                     <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
                     <!-- 
-                      错误原因：goods一开始{}  {}.categories -> undefined  -> undefined[1]
-                      1. 可选链的语法?. 
-                      goods.categories?.[1].id}
-                      2. v-if手动控制渲染时机 保证只有数据存在才渲染
-                     -->
+                                  错误原因：goods一开始{}  {}.categories -> undefined  -> undefined[1]
+                                  1. 可选链的语法?. 
+                                  goods.categories?.[1].id}
+                                  2. v-if手动控制渲染时机 保证只有数据存在才渲染
+                                 -->
                     <el-breadcrumb-item :to="{ path: `/category/${goods.categories[1].id}` }">{{ goods.categories[1].name }}
                     </el-breadcrumb-item>
                     <el-breadcrumb-item :to="{ path: `/category/sub/${goods.categories[0].id}` }">{{
@@ -124,7 +124,10 @@ onMounted(() => getGoods())
                         </div>
                         <!-- 24热榜+专题推荐 -->
                         <div class="goods-aside">
-
+                            <!-- 24小时 -->
+                            <DetailHot />
+                            <!-- 周 -->
+                            <DetailHot />
                         </div>
                     </div>
                 </div>
